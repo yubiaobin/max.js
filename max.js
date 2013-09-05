9c77a32c24890e57bc45e6474785c1ee9de0135b
yubiaobin <yubiaobin1986@gmail.com>
2013-09-05 11:18:40 +0800

    javascript

---
+++ max.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @authors max_yu(yubiaobin1986@gmial.com)
+ * @date    2013-09-05 10:02:41
+ * @version 1.2
+ */
+
+
+var Max = {
+	getId : function(id){return document.getElementById(id)},
+	getName : function(name){return document.getElementsByName(name)},
+	getTagName : function(tag){return document.getElementsByTagName(tag)}
+};
